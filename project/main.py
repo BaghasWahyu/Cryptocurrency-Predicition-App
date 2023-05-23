444a5199ba02c6bdde80bf0d033670c098b4d14d
Baghas Wahyu <82638738+BaghasWahyu@users.noreply.github.com>
2023-05-23 19:23:00 +0700

    Update main.py

--- project/main.py
+++ project/main.py
@@ -84,8 +84,8 @@ if len(dropdown) > 0:
         f"./data_historis/{dropdown}_data_historis.xlsx", index_col=0, parse_dates=True)
     df = pd.DataFrame(data_historis)
 
-    with open(f"./data_historis/{dropdown}_data_historis.xlsx", "r") as file_historis:
-        btn_historis_data = st.download_button(
+    with open(f"./data_historis/{dropdown}_data_historis.xlsx", "rb") as file_historis:
+        st.download_button(
             label="Download Data Historis",
             data=file_historis,
             file_name=f'{dropdown}_data_historis.xlsx',
