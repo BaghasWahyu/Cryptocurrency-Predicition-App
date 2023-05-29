16eb7610ef2d4d1645dd3e4225e78879457c4b63
Baghas Wahyu <82638738+BaghasWahyu@users.noreply.github.com>
2023-05-29 21:03:03 +0700

    Update main.py

--- project/main.py
+++ project/main.py
@@ -99,7 +99,7 @@ if len(dropdown) > 0:
         "Pilih Aspek untuk ditampilkan dalam bentuk Line Chart", cols1_5, key='chart_crypto_2')
     if pilihan1_5:
         data1_5 = df[pilihan1_5]
-        st.line_chart(data1_5, x=df["Date"], y=pilihan1_5)
+        st.line_chart(data1_5, x=data1_5.index, y=pilihan1_5)
     else:
         st.warning('Silahkan Pilih Aspek yang akan Ditampilkan Terlebih Dahulu!')
 
