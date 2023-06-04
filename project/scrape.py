c9e910f69d8ea982e78ca34175a908660fae81a9
Baghas Wahyu <82638738+BaghasWahyu@users.noreply.github.com>
2023-06-04 19:09:18 +0700

    Update scrape.py

--- project/scrape.py
+++ project/scrape.py
@@ -61,8 +61,7 @@ def get_symbol():
     return nama_crypto, symbol_crypto
 
 
-if __name__ == "__main__":
-    nama_crypto, symbol_crypto = get_symbol()
+nama_crypto, symbol_crypto = get_symbol()
 
 symbolCrypto = []
 
